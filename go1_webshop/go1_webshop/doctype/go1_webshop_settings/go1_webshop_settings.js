18afecb18be00d7aa8173ee64510af4592c89e64
Tridotstech <info@tridotstech.com>
2024-03-08 14:01:51 +0000

    updates

--- go1_webshop/go1_webshop/doctype/go1_webshop_settings/go1_webshop_settings.js
+++ go1_webshop/go1_webshop/doctype/go1_webshop_settings/go1_webshop_settings.js
@@ -5,22 +5,37 @@ frappe.ui.form.on("Go1 Webshop Settings", {
 	refresh(frm) {
 	},
        import_all_pages(frm){
-        console.log('import_all_pages')
-             
+        console.log('import_all_pages',$("#loader"))
+        
+        if( $("#loader").length != 0){
+              console.log('sssss');
+              $("#loader").css("display", "block");
+        }else{
+              console.log('aaaaaa  ');
+              let loader = document.createElement('p')
+              loader.id = 'loader'
+              loader.textContent = 'Loading...'
+              document.querySelector('[data-fieldtype=Button]').parentNode.append(loader)
+        }
+             $('[data-fieldtype=Button]').hide()  
               frappe.call({
                      method:'go1_webshop.go1_webshop.after_install.insert_pages',
                      args:{
                      },
                      // freeze:true,
                      callback: function(r) {
+                            $('[data-fieldtype=Button]').show()                           
+                            $('#loader').hide()
+                            frappe.msgprint(__('Pages imported successfully'));
                             if(r && r.message == 'success'){
-                                   // console.log('llll');
+                                   // console.log('llll');                                 
                                    frappe.msgprint(__('Pages imported successfully'));
                             }else{
                                    alert('Something Went Wrong')
                             }
                      }
               })
+              
        }
 });
 
